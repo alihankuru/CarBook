a6e5de05fc689ccb1904da15057336c6f1efeab3
AK <ali.kuru6@hotmail.com>
2024-04-14 01:40:54 +0300

    -

---
+++ Frontends/CarBook.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult AdminHeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminNavbarPartial()
+        {
+            return PartialView();
+        }
+
+    }
+}
