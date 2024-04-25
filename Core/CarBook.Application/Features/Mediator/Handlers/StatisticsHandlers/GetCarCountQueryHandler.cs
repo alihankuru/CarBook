480d81bf4806c224e9df55c87f3ddfd374629dbf
AK <ali.kuru6@hotmail.com>
2024-04-26 01:25:40 +0300

    Add Statistics

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/StatisticsHandlers/GetCarCountQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.Application.Features.CQRS.Queries;
+using CarBook.Application.Features.Mediator.Queries.StatisticsQueries;
+using CarBook.Application.Features.Mediator.Results.StatisticsResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.StatisticsHandlers
+{
+    public class GetCarCountQueryHandler : IRequestHandler<GetCarCountQuery, GetCarCountQueryResult>
+    {
+        private readonly IRepository<Car> _repository;
+
+        public GetCarCountQueryHandler(IRepository<Car> repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<GetCarCountQueryResult> Handle(GetCarCountQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
