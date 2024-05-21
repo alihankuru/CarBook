d9f0264b89cb1d9f16561e3e5d179d5dcfff6e7e
AK <ali.kuru6@hotmail.com>
2024-05-21 23:22:12 +0300

    -

--- Frontends/CarBook.WebUI/Controllers/RentACarListController.cs
+++ Frontends/CarBook.WebUI/Controllers/RentACarListController.cs
@@ -6,8 +6,21 @@ namespace CarBook.WebUI.Controllers
     {
         public IActionResult Index()
         {
-            var data = TempData["value"];
-            ViewBag.v = data;
+         
+
+            var bookpickdate = TempData["bookpickdate"];
+            var bookoffdate = TempData["bookoffdate"];
+            var timepick = TempData["timepick"];
+            var timeoff = TempData["timeoff"];
+            var locationID = TempData["locationID"];
+
+            ViewBag.bookpickdate = bookpickdate;
+            ViewBag.bookoffdate = bookoffdate;
+            ViewBag.timepick = timepick;
+            ViewBag.timeoff = timeoff;
+            ViewBag.locationID = locationID;
+
+
 
             return View();
         }
