df68c45b1d449a053674712289330693a9f94775
AK <ali.kuru6@hotmail.com>
2024-05-29 01:46:40 +0300

    -

---
+++ Frontends/CarBook.WebUI/ViewComponents/DashboardComponents/_AdminDashboardBlogListComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.DashboardComponents
+{
+    public class _AdminDashboardBlogListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _AdminDashboardBlogListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7290/api/Blogs/GetAllBlogsWithAuthorsList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBlogDto>>(jsonData);
+
+                return View(values);
+
+            }
+            return View();
+
+            
+        }
+    }
+}
